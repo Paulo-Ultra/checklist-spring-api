547c6cbd169934fbf10058bbc6c59be7271e1045
Paulo Ricardo Freire Ultra <prfultra@yahoo.com.br>
2023-10-27 13:43:22 -0300

    Repository Anotation

--- src/main/java/com/learning/springboot/checklistapi/repository/ChecklistItemRepository.java
+++ src/main/java/com/learning/springboot/checklistapi/repository/ChecklistItemRepository.java
@@ -2,10 +2,12 @@ package com.learning.springboot.checklistapi.repository;
 
 import com.learning.springboot.checklistapi.entity.ChecklistItemEntity;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface ChecklistItemRepository extends PagingAndSortingRepository<ChecklistItemEntity, Long> {
 
     Optional<ChecklistItemEntity> findByGuid(String guid);
