40b4a55a1c0f1d7e9685b7bf6fbc69d0b29fde6b
Paulo Ricardo Freire Ultra <prfultra@yahoo.com.br>
2023-10-27 13:54:07 -0300

    Repository JpaRepository

--- src/main/java/com/learning/springboot/checklistapi/repository/CategoryRepository.java
+++ src/main/java/com/learning/springboot/checklistapi/repository/CategoryRepository.java
@@ -1,13 +1,13 @@
 package com.learning.springboot.checklistapi.repository;
 
 import com.learning.springboot.checklistapi.entity.CategoryEntity;
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface CategoryRepository extends PagingAndSortingRepository <CategoryEntity, Long> {
+public interface CategoryRepository extends JpaRepository<CategoryEntity, Long> {
 
     Optional<CategoryEntity> findByGuid(String guid);
     Optional<CategoryEntity> findByName(String name);
--- src/main/java/com/learning/springboot/checklistapi/repository/ChecklistItemRepository.java
+++ src/main/java/com/learning/springboot/checklistapi/repository/ChecklistItemRepository.java
@@ -1,14 +1,14 @@
 package com.learning.springboot.checklistapi.repository;
 
 import com.learning.springboot.checklistapi.entity.ChecklistItemEntity;
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface ChecklistItemRepository extends PagingAndSortingRepository<ChecklistItemEntity, Long> {
+public interface ChecklistItemRepository extends JpaRepository<ChecklistItemEntity, Long> {
 
     Optional<ChecklistItemEntity> findByGuid(String guid);
 
