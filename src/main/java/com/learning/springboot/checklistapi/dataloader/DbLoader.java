79b98aa8c6e4b4934fe09d8068437dc0ab2b3fd7
Paulo Ricardo Freire Ultra <prfultra@yahoo.com.br>
2023-10-29 17:02:10 -0300

    DB Loader

---
+++ src/main/java/com/learning/springboot/checklistapi/dataloader/DbLoader.java
@@ -0,0 +1,42 @@
+package com.learning.springboot.checklistapi.dataloader;
+
+import com.learning.springboot.checklistapi.entity.CategoryEntity;
+import com.learning.springboot.checklistapi.repository.CategoryRepository;
+import com.learning.springboot.checklistapi.service.CategoryService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+@Profile("data-load")
+@Slf4j
+@Component
+public class DbLoader implements CommandLineRunner {
+
+    private final CategoryRepository categoryRepository;
+    private final CategoryService categoryService;
+
+    public DbLoader(CategoryRepository categoryRepository, CategoryService categoryService){
+        this.categoryRepository = categoryRepository;
+        this.categoryService = categoryService;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        log.info("Populating db with categories");
+        List<String> categoryNames = Arrays.asList(
+                "Trabalho", "Saúde", "Educação", "Pessoal", "Outros"
+        );
+
+        for(String categoryName : categoryNames){
+            Optional<CategoryEntity> catOpt = this.categoryRepository.findByName(categoryName);
+           if(!catOpt.isPresent()){
+               categoryService.addNewCategory(categoryName);
+           }
+        }
+    }
+}
